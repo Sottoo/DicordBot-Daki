075ccba4c9dfd3e2243e34b318ac4560ea70b997
Angel Soto <angelsotomejia04@gmail.com>
2026-05-19 12:09:58 -0600

    feat: agregar estado personalizado al bot

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -1,4 +1,4 @@
-import { Events } from 'discord.js';
+import { Events, ActivityType } from 'discord.js';
 import { CustomClient } from '../index.js';
 
 export default {
@@ -6,5 +6,15 @@ export default {
     once: true,
     execute(client: CustomClient) {
         console.log(`Ready! Logged in as ${client.user?.tag}`);
+        
+        // Configura el Rich Presence / Estado personalizado
+        client.user?.setPresence({
+            activities: [{
+                name: 'customstatus',
+                type: ActivityType.Custom,
+                state: 'Moderando el servidor de Daki'
+            }],
+            status: 'online'
+        });
     }
 };
